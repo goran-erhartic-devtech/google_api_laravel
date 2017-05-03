e5eea8854650bd2bee11141df399c10e0d2c94fc
goran-erhartic-devtech <goran.erhartic@devtechgroup.com>
2017-05-03 15:59:29 +0200

    Added Search input feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,136 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Google MAP</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <style>
+        #map {
+            height: 500px;
+            width: 700px;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        #pac-input {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+<h2>Distance from DevTech: <label id="res"></label></h2>
+<input id="pac-input" class="controls" type="text" placeholder="Search Box">
+<div id="map"></div>
+
+<script>
+
+    var markers = [];
+    var lines = [];
+
+    function clearMarkerAndLine() {
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+        lines.forEach(function (line) {
+            line.setMap(null);
+        });
+        lines = [];
+    }
+
+    function createMarker(map, searchBounds, devtech, location) {
+        markers.push(new google.maps.Marker({
+            map: map,
+            title: "My Location",
+            position: location
+        }));
+
+        var lineCoordinates = [
+            devtech,
+            {lat: location.lat(), lng: location.lng()}
+        ];
+        var aerialDistance = new google.maps.Polyline({
+            path: lineCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        aerialDistance.setMap(map);
+        lines.push(aerialDistance);
+
+        var newPosition = new google.maps.LatLng(location.lat(), location.lng());
+        if (searchBounds.contains(newPosition)) {
+            //calculates distance between two points in km's
+            function calcDistance(p1, p2) {
+                return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
             }
 
-            .full-height {
-                height: 100vh;
-            }
+            document.getElementById('res').innerHTML = calcDistance(devtech, newPosition) + "km";
+        } else {
+            document.getElementById('res').innerHTML = '';
+            alert('Out of bounds...')
+        }
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    function initMap() {
+        //begin map setup
+        var devtech = new google.maps.LatLng(45.25345870690877, 19.845862984657288);
 
-            .position-ref {
-                position: relative;
-            }
+        var searchBounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(45.229425102968776, 19.774188995361328),
+            new google.maps.LatLng(45.28029949181387, 19.882678985595703)
+        );
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        var map = new google.maps.Map(document.getElementById('map'));
 
-            .content {
-                text-align: center;
-            }
+        var marker = new google.maps.Marker({
+            position: devtech,
+            map: map,
+            title: "DevTech DOO"
+        });
 
-            .title {
-                font-size: 84px;
-            }
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        var viewBounds = {
+            north: 45.23747,
+            south: 45.26756,
+            east: 19.85218,
+            west: 19.79433
+        };
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+        map.fitBounds(viewBounds);
+        //end map setup
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        //Click on map
+        google.maps.event.addListener(map, 'click', function (event) {
+            clearMarkerAndLine();
+            createMarker(map, searchBounds, devtech, event.latLng);
+        });
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+        //Search address
+        searchBox.addListener('places_changed', function () {
+            var places = searchBox.getPlaces();
+
+            clearMarkerAndLine();
+            createMarker(map, searchBounds, devtech, places[0].geometry.location);
+        });
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXSN5-KefqORJYNTpUxuIxERpAiiW4uek&callback=initMap&libraries=places,geometry"
+        async
+        defer></script>
+</body>
 </html>
